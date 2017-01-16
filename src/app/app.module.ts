8d652e6d0b5c46f122efb8517616719c0d710729
Vicens Fayos <datracka@gmail.com>
2017-01-16 18:20:07 +0100

    add commumity and sponsors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MarkdownModule} from 'angular2-markdown';
 
 import { AppComponent } from './app.component';
 import { IndicatorComponent } from './indicator/indicator.component';

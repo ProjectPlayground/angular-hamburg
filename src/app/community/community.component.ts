f16a1072330bb6d4b28c41c696522f8e19dd71af
Vicens Fayos <datracka@gmail.com>
2017-02-04 14:56:23 +0100

    fix community

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-community',
   templateUrl: './community.component.html',
-  styleUrls: ['font-awesome.min.css','./community.component.css'],
+  styleUrls: ['./community.component.css'],
   encapsulation: ViewEncapsulation.Native
 })
 export class CommunityComponent implements OnInit {
--- src/app/link/link.component.ts
+++ src/app/link/link.component.ts
@@ -3,12 +3,12 @@ import {Component, OnInit, ViewEncapsulation, Input} from '@angular/core';
 @Component({
   selector: 'app-link',
   templateUrl: './link.component.html',
-  styleUrls: ['../variables.css', './link.component.css'],
+  styleUrls: ['./font-awesome.min.css', '../variables.css', './link.component.css'],
   encapsulation: ViewEncapsulation.Native
 })
 export class LinkComponent implements OnInit {
 
-  @Input() public className: string = '';
+  @Input() public class: string = '';
   @Input() public target: string = '';
   @Input() public href: string = '';
   @Input() public title: string = '';

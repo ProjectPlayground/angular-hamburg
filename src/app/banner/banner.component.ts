94065291b4822509245e986825aec1a6e17b9a1e
Vicens Fayos <datracka@gmail.com>
2017-02-04 16:00:27 +0100

    remove animation from banner

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -4,16 +4,7 @@ import {Component, style, ViewEncapsulation, state, animate, transition, trigger
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css'],
-  encapsulation: ViewEncapsulation.Emulated,
-  animations: [
-    trigger('flyInOut', [
-      state('in', style({opacity: 1})),
-      transition('void => *', [
-        style({opacity: 0}),
-        animate(300)
-      ])
-    ])
-  ]
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class BannerComponent {
 
